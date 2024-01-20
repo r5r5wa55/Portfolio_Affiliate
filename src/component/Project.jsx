748ba6af7dc22bb50b488a799317057c80782064
werapol <r5r5wa55@hotmail.com>
2024-01-20 23:53:14 +0700

    แคิด

---
+++ src/component/Project.jsx
@@ -0,0 +1,49 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function Project() {
+  return (
+    <div>
+        <div className='bg-wecolor h-60 flex flex-col justify-center items-center'>
+        <h2 className='font-bold text-2xl mb-5'>Future</h2>
+        <h1 className='xl:text-6xl md:text-6xl sm:text-6xl font-bold text-blue-400 m-0 flex text-3xl'>แนวคิด<div className="mx-5 text-red-400">&</div> ทัศนวิสัย</h1>
+        </div>
+        <div className="mb-10 flex justify-center ">
+          <div className="w-10/12 h-full grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-5 justify-items-center">
+            <div className="w-full flex justify-center items-center">
+              <div className="bg-blue-400 w-full h-[300px] m-5 rounded-lg">
+                <div className="w-full">we</div>
+              </div>
+            </div>
+            <div className="w-full flex justify-center items-center  ">
+              <div className="bg-blue-400 w-full h-[300px] m-5 rounded-lg">
+                <div className="w-full">we</div>
+              </div>
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <div className="bg-blue-400 w-full h-[300px] m-5 rounded-lg">
+                <div className="w-full">we</div>
+              </div>
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <div className="bg-blue-400 w-full h-[300px] m-5 rounded-lg">
+                <div className="w-full">we</div>
+              </div>
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <div className="bg-blue-400 w-full h-[300px] m-5 rounded-lg">
+                <div className="w-full">we</div>
+              </div>
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <div className="bg-blue-400 w-full h-[300px] m-5 rounded-lg">
+                <div className="w-full">we</div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+    </div>
+    
+  )
+}
--- src/component/Rout.jsx
+++ src/component/Rout.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
+import Project from './Project'
 
 export default function Rout() {
   return (
@@ -11,6 +12,7 @@ export default function Rout() {
       <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
+          <Route path='/project' element={<Project />}/>
       </Routes>   
     </>
   )
